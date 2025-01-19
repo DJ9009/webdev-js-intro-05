b78b7a00b94d96be5b87f43d2715a00eec4749d0
David Joslin <davidjoslin1990@gmail.com>
2025-01-19 02:40:13 -0500

    updated assignment js and index js

--- js/assignment.js
+++ js/assignment.js
@@ -9,7 +9,8 @@ const todos = [
 ];
 
 // Declare your two immutable variables
-
+const shiftvalue;
+const popvalue;
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -22,46 +23,59 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+    answerOneEl.textContent = 'First item: ${todo[0]}'
 }
 
 function updateAnswerTwo() {
-
+    answerTwoEl.textContent = `Last item: ${todos[todos.length - 1]}`;
 }
 
 function removeLastValue() {
-
+    popValue = todos.pop();
+    updateAnswerTwo();
 }
 
 function removeFirstValue() {
-
+    shiftValue = todos.shift();
+    updateAnswerOne();
 }
 
 function addShiftAndPopValues() {
-
+    todos.push("Complete the assignment");
+    updateAnswerFive();
 }
 
 function updateAnswerFour() {
+    answerFourEl.textContent = `Array length: ${todos.length}`;
 
 }
 
 function reverseTodoList() {
-
+    todos.reverse();
+    updateAnswerSix();
 }
 
 function updateAnswerFive() {
-
+    answerFiveEl.textContent = `Updated Todo list: ${todos.join(", ")}`;
 }
 
 function updateAnswerSix() {
-
+    answerSixEl.textContent = `Reversed Todo list: ${todos.join(", ")}`;
 }
 
 function render() {
-
+    updateAnswerOne();
+    updateAnswerTwo();
+    updateAnswerFour();
+    updateAnswerFive();
+    updateAnswerSix();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
+    removeLastValue();
+    removeFirstValue();
+    addShiftAndPopValues()
+    reverseTodoList();
     render();
 });
--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+// let mixedArray = [1, "Hello", true, {name: "John"}];
+// console.log(mixedArray);
+// console.log(typeof mixedArray);
+// mixedArray.shift();
+// console.log(mixedArray);
+// mixedArray.push("New Item");
+// console.log(mixedArray);
+// mixedArray[1] = "New Value";
+// console.log(mixedArray);
